90ff87d1ffdc434a3e58dc0d2a9360380e89ca8d
feronetick <dms@itgro.ru>
2019-05-17 00:48:04 +0500

    Вроде работает

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,66 +14,55 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   static Point _point = Point(56.837626, 60.597405);
   YandexMapController _yandexMapController;
-  Placemark _placemark = Placemark(
-    point: _point,
-    icon: 'lib/assets/place.png',
-    onTap: (latitude, longitude) => print('Tapped me at $latitude,$longitude')
-  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('YandexMapkit Plugin')
-        ),
-        body: Column(
-          children: [
-            Expanded(
-              child: YandexMap(
-                onCreated: (YandexMapController controller) async {
-                  _yandexMapController = controller;
+        home: Scaffold(
+      body: Column(children: [
+        Expanded(child: YandexMap(
+          onCreated: (YandexMapController controller) async {
+            _yandexMapController = controller;
 
-                  Point defaultPoint = new Point(56.837626, 60.597405);
+            Point defaultPoint = new Point(56.837626, 60.597405);
 
-                  List<Point> points = [
-                    new Point(
-                      defaultPoint.latitude + 0.0015,
-                      defaultPoint.longitude - 0.0015,
-                    ),
-                    new Point(
-                      defaultPoint.latitude - 0.0015,
-                      defaultPoint.longitude - 0.0015,
-                    ),
-                    new Point(
-                      defaultPoint.latitude,
-                      defaultPoint.longitude + 0.0015,
-                    ),
-                  ];
+            List<Point> points = [
+              new Point(
+                defaultPoint.latitude + 0.0015,
+                defaultPoint.longitude - 0.0015,
+              ),
+              new Point(
+                defaultPoint.latitude - 0.0015,
+                defaultPoint.longitude - 0.0015,
+              ),
+              new Point(
+                defaultPoint.latitude,
+                defaultPoint.longitude + 0.0015,
+              ),
+            ];
 
-                  controller.addPolygon(
-                    points: points,
-                    fillColor: Color(0xFF70C3BE).withOpacity(0.51),
-                    strokeColor: Color(0xFFF9F4F4),
-                    strokeWidth: 1,
-                    zIndex: 10,
-                  );
+            controller.addPolygon(
+              points: points,
+              fillColor: Color(0xFF70C3BE).withOpacity(0.51),
+              strokeColor: Color(0xFFF9F4F4),
+              strokeWidth: 1,
+              zIndex: 10,
+            );
 
-                  controller.onCameraPositionChanged.listen((CameraPositionEvent event) {
-                    print(event);
-                  });
+            controller.onCameraPositionChanged
+                .listen((CameraPositionEvent event) {
+              print(event);
+            });
 
-                  await controller.move(
-                    point: _point,
-                    animation: MapAnimation(smooth: true, duration: 2.0),
-//                      zoom: 14.4,
-                  );
-                },
-              )
-            )
-          ]
-        ),
-      )
-    );
+            await controller.move(
+              point: _point,
+              animation: MapAnimation(
+                smooth: true,
+              ),
+            );
+          },
+        ))
+      ]),
+    ));
   }
 }
--- lib/src/map_animation.dart
+++ lib/src/map_animation.dart
@@ -1,8 +1,30 @@
 class MapAnimation {
-  final double duration;
+  final Duration duration;
   final bool smooth;
 
-  static const double kAnimationDuration = 2.0;
+  const MapAnimation({
+    this.smooth = true,
+    this.duration = const Duration(seconds: 1),
+  });
 
-  const MapAnimation({this.smooth = true, this.duration = kAnimationDuration});
+  Map toMap() {
+    return {
+      "smooth": smooth,
+      "duration": duration.inMilliseconds,
+    };
+  }
+
+  @override
+  String toString() => 'MapAnimation{duration: $duration, smooth: $smooth}';
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is MapAnimation &&
+          runtimeType == other.runtimeType &&
+          duration == other.duration &&
+          smooth == other.smooth;
+
+  @override
+  int get hashCode => duration.hashCode ^ smooth.hashCode;
 }
